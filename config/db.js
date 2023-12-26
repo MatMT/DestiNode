a292b63dbdc8ff2e1b4382ddc55330bcbfb11233
Mateo Elias <oscarmateoelias@gmail.com>
2023-12-26 09:49:14 -0600

    Corrección en pool DB

--- config/db.js
+++ config/db.js
@@ -7,7 +7,7 @@ const db = new Sequelize(process.env.DATABASE_URL, {
         timestamps: false
     },
     pool: {
-        max: 5,
+        max: 3,
         min: 0,
         acquire: 30000,
         idle: 10000
